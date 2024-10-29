f11c507061748670ea0f40f99dbdf5e85ded97a4
Ani <hazraaniruddha.publishwebsite@gmail.com>
2024-10-30 01:10:12 +0530

    v1.0.2

--- server.js
+++ server.js
@@ -3,14 +3,22 @@ const PORT = process.env.PORT || 3000;
 const io = require("socket.io")(PORT, {
     cors: {
         // origin: ["http://localhost:8080"]
-        origin: "*"
-    }
-})
+        origin: "*",
+    },
+});
 
-io.on("connection", socket => {
-    console.log(socket.id);
-    socket.broadcast.emit("new-user-joined", socket.id);
-    socket.on("send-message-server", message => {
-        socket.broadcast.emit("send-message-client", socket.id, message);
-    })
-})
+io.on("connection", (socket) => {
+    socket.stateObj = {};
+    socket.on("update-stateObj-server", (newObj) => {
+        for (const key in newObj) {
+            socket.stateObj[key] = newObj[key];
+        }
+        socket.broadcast.emit("new-user-joined", socket.stateObj.name);
+    });
+    socket.on("send-message-server", (name, message) => {
+        socket.broadcast.emit("send-message-client", name, message);
+    });
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("disconnect-client", socket.stateObj.name);
+    });
+});
